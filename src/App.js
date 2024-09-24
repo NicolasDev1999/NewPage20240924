8444dee27e9aadd38bd46874bd2b7f655c309ac6
NicolasDev1999 <sergionicolasgalindocante@gmail.com>
2024-09-24 16:29:05 -0500

    change url

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         
         <main className="container">
           <Routes>
-            <Route path="/Page202409/build/index.html" element={<Banner />} />
+            <Route path="/" element={<Banner />} />
             <Route path="/Page202409/build" element={<Banner />} />
             <Route path="/Banner" element={<Banner />} />
             <Route path="/About" element={<About/>} />
